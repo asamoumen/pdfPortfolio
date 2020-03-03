f8174e9e09724cf1409522d2a79cf12fd7a5d147
Abdelmoumene Ait Si Ahmad <abdelmoumene.aitsiahmad@devoteam.lu>
2020-03-03 10:34:09 +0100

    added file encode

--- Pdf Manager/src/com/abdel/PdfManager.java
+++ Pdf Manager/src/com/abdel/PdfManager.java
@@ -1,15 +1,24 @@
-package com.pdfmanager;
+package com.abdel;
 import com.aspose.pdf.Document;
 import com.aspose.pdf.FileSpecification;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 
 public class PdfManager {
+	
 
 	public  PdfManager() {
 		// TODO Auto-generated method stub
 		 
 	}
 	
-	public static String generatePDF() {
+	public static String generatePDF() throws IOException {
 		//Instantiate Document Object
 	 	Document doc = new com.aspose.pdf.Document();
 	 	//Instantiate document Collection object
@@ -35,9 +44,20 @@ public class PdfManager {
 	 	System.out.println(pdf.getName());
 	 	System.out.println("inside first method");
 	 	//Save Portfolio document
-	 	doc.save("PortFoliio_output.pdf");
+	 	doc.save("PortFoliio_output.pdf");	 
 	 	
-	 	return "ok";
+	 	return encode("PortFoliio_output.pdf");
+	}
+	
+	public static String encode(String filePath) throws IOException {
+		 	 
+		    File pdfFile = new File(filePath);
+		    byte[] encoded = Files.readAllBytes(Paths.get(pdfFile.getAbsolutePath()));
+		    Base64.Encoder enc = Base64.getEncoder();
+		    byte[] strenc = enc.encode(encoded);
+		    String encode = new String(strenc, "UTF-8");
+		    
+		    return encode;
 	}
 
 }
